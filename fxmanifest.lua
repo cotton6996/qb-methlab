e8dd040f4a631e718f34e46faef3b9711ca5512c
El Peaky <63703372+SneakyPanda998@users.noreply.github.com>
2022-01-17 07:27:01 -0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+--Do not repost without 90zPeaky's Permission--
+--Redone by 90zPeaky
+
+--Redone source code by MrEvilGamer - https://github.com/MrEvilGamer/qb-weedpicking
+--Original Script author MrEvilGamer
+
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'qb-methlab'
+
+server_scripts {
+	
+	'config.lua',
+	'server/meth.lua'
+}
+
+client_scripts {
+
+	'config.lua',
+	'client/meth.lua'
+}
+
+lua54 'yes'
